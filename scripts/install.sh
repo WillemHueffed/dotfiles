16237c4ffc9ad96c28a941c2038161d44caf9026
Willem <willmhueffed@gmail.com>
2024-02-21 21:36:07 -0800

    Adds stow to install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,5 @@
+sudo apt install stow
 sudo apt install tmux
 sudo apt install neovim
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
+git clone https://github.com/NvChad/NvChad ~/dotfiles/.config/nvim --depth 1 && nvim
