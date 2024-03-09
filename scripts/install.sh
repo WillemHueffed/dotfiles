0828453bb3c13606cc67072c90fa0b97dfb52726
Willem Hueffed <willmhueffed@gmail.com>
2024-03-09 14:08:47 -0800

    updates install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,12 @@
 sudo apt install stow
 sudo apt install tmux
-sudo apt install neovim
+
+# Install latest release of nvim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+sudo mv nvim.appimage /usr/bin
+
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone -b v2.0 https://github.com/NvChad/NvChad ~/dotfiles/.config/nvim --depth 1 && nvim
+
+
