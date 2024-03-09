9aa413ea041d3ad9b5d4a44df45423aa1f8b8ce1
Willem Hueffed <willmhueffed@gmail.com>
2024-03-09 14:05:05 -0800

    updates target branch for nvchad

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,4 +2,4 @@ sudo apt install stow
 sudo apt install tmux
 sudo apt install neovim
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-git clone https://github.com/NvChad/NvChad ~/dotfiles/.config/nvim --depth 1 && nvim
+git clone -b v2.0 https://github.com/NvChad/NvChad ~/dotfiles/.config/nvim --depth 1 && nvim
