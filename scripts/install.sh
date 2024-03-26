621bf8e7a66cafc98ede055a68f9095e092a78d3
Will <56740451+WillemHueffed@users.noreply.github.com>
2024-03-25 17:30:52 -0700

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,21 +2,30 @@ sudo apt install stow
 
 # Install Tmux
 sudo apt install tmux
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # Install tmux package manager
+#git clone https://github.com/tmux-plugins/tpm ~/dotfiles/.configs/tmux/plugs/
 # leader (crtl+space) + I -> (re)load env/packages
-#mkdir ../.config/tmux/plugins/tpm/
-#git clone git clone https://github.com/tmux-plugins/tpm ../.config/tmux/plugs/tpm/
+
+# Install Node.js v21.x
+curl -fsSL https://deb.nodesource.com/setup_21.x | sudo -E bash - &&\
+sudo apt-get install -y nodejs
+
+# Install python virtual environment for nvim-mason
+sudo apt install python3-venv
 
 # Install latest release of nvim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 sudo mv nvim.appimage /usr/bin/nvim
 
+# Run installations
+stow ~/dotfiles/.
+tmux
+tmux source ~/.config/tmux/tmux.conf
+nvim --headless -c ":MasonInstallAll"
+
 # Install NvChad
 #git clone -b v2.0 https://github.com/NvChad/NvChad ~/dotfiles/.config/nvim --depth 1 && nvim
 
-# Install Node.js v21.x
-curl -fsSL https://deb.nodesource.com/setup_21.x | sudo -E bash - &&\
-sudo apt-get install -y nodejs
+
